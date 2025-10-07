738c9505a7c85d332e47d23ac9194adb91afe50d
Arnold Campos <109268701+arnoldcamposcn@users.noreply.github.com>
2025-10-07 16:10:05 -0500

    vite

--- src/components/molecules/ForoInlineComments.tsx
+++ src/components/molecules/ForoInlineComments.tsx
@@ -178,3 +178,4 @@ export const ForoInlineComments: React.FC<ForoInlineCommentsProps> = ({
     </div>
   );
 };
+
--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // 🛑 Añadir esta sección para permitir el acceso desde el subdominio 🛑
   server: {
-    watch: {
-      usePolling: true,
-    },
-  },
-})
+    // Si estás usando Docker, es una buena práctica forzar el host
+    host: '0.0.0.0', 
+    
+    // Permitir el acceso desde tu dominio público
+    allowedHosts: [
+      'revista.metaminingmedia.com', 
+      '127.0.0.1', 
+      'localhost',
+    ]
+  }
+  // Fin de la sección añadida
+});
